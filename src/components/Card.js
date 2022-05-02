16d2bdcae263323380d845bf0847a0a6c2a43f33
Jeetpal <singhjeetpal99@gmail.com>
2022-05-02 17:18:21 -0500

    Added more cards dynamically, it just reads the data from the data.js and created the cards without hard coding

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,29 @@
 import React from "react";
 import star from "../images/star.png";
 function Card(props) {
+  let badgeText;
+  if (props.openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (props.location === "Online") {
+    badgeText = "ONLINE";
+  }
   return (
     <div className="card">
-      {props.image}
+      {badgeText && <div className="card--badge">{badgeText}</div>}
+      <img
+        src={`images/${props.coverImg}`}
+        className="card--image"
+        alt="experience pic"
+      />
       <div className="card--title">
         <img src={star} alt="star logo" className="card--star" />
-        <span className="grey">{props.rating}</span>
-        <p className="card--details">{props.details}</p>
-        <p className="card--price">{props.price}</p>
+        <span className="grey">
+          {props.stats.rating} ({props.stats.reviewCount}) • {props.location}
+        </span>
+        <p className="card--details">{props.title}</p>
+        <p className="card--price">
+          <span className="bold">From ${props.price}</span> / person
+        </p>
       </div>
     </div>
   );
--- src/components/Experiences.js
+++ src/components/Experiences.js
@@ -1,65 +1,17 @@
 import React from "react";
 import Card from "./Card";
-import pic1 from "../images/pic1.png";
-import pic2 from "../images/pic2.png";
-import pic3 from "../images/pic3.png";
+import data from "../data";
 
 function Experiences() {
-  let img1 = <img src={pic1} alt="experience" className="experience" />;
-  let img2 = <img src={pic2} className="experience" />;
-  let img3 = <img src={pic3} className="experience" />;
-
-  let allData = [
-    {
-      image: { img1 },
-      rating: "5.0(0) * USA",
-      details: "Life lessons with Katie Zaferes",
-      price: "From $136 / person",
-    },
-    {
-      image: { img2 },
-      rating: "5.0(30) * USA",
-      details: "Learn wedding photography",
-      price: "From $125 / person",
-    },
-    {
-      image: { img3 },
-      rating: "4.8(2) * USA",
-      details: "Group Mountain Biking",
-      price: "From $50 / person",
-    },
-  ];
-
-  return (
-    <div className="experiences">
+  let cards = data.map((experience) => {
+    return (
       <Card
-        image={img1}
-        rating="4.8(2) * USA"
-        details="Group Mountain Biking"
-        price="From $50 / person"
+        key={experience.id}
+        {...experience}
       />
-      <Card
-        image={img2}
-        rating="5.0(30) * USA"
-        details="Learn wedding photography"
-        price="From $125 / person"
-      />
-      <Card
-        image={img3}
-        rating="4.8(2) * USA"
-        details="Group Mountain Biking"
-        price="From $50 / person"
-      />
-      {/* {this.allData.map((card)=>{
-            return (<Card 
-                image = {card.image}
-                rating = {card.rating}
-                details = {card.details}
-                price = {card.price}
-            /> )
-        })} */}
-    </div>
-  );
+    );
+  });
+  return <div className="experiences">{cards}</div>;
 }
 
 export default Experiences;
